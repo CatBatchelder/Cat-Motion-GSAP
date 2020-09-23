6393fd1e958ee40b45d23d5154739f7a091401b1
CatBatchelder <63326220+CatBatchelder@users.noreply.github.com>
2020-09-23 16:57:34 -0400

    finished gsap demo

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,10 +1,18 @@
 import {gsap} from "gsap";
 
-gsap.to("#star", {
-    //alpha: 0,
-    duration: 4,
-    repeat: -1,
-    color: "#ffa500",
-    rotation: 365
-    //transformOrigin:"50% 50%"
-});
+var starTimeline = gsap.timeline({repeat: -1});
+
+starTimeline.to("#star", {duration: 4, color: "#ffa500", rotation: 360, x: "500px"})
+            .to("#star", {duration: 4, color: "#0000ff", rotation: -360, x: "0px"});
+
+
+
+// gsap.to("#star", {
+//     //alpha: 0,
+//     duration: 4,
+//     repeat: -1,
+//     color: "#ffa500",
+//     rotation: 365,
+//     x: "500px"
+//     //transformOrigin:"50% 50%"
+// });
