fb5091fb502acaa8675f2515d6db9bee692918cb
CatBatchelder <63326220+CatBatchelder@users.noreply.github.com>
2020-09-29 15:31:50 -0400

    trying to fix it

--- dev/js/stars/star1.js
+++ dev/js/stars/star1.js
@@ -15,8 +15,8 @@ console.log("is star 1 working");
 export function star1Animation(){
     var tl = gsap.timeline();
 
-    tl.to("#star1", {duration: 1, alpha: .5, scale: 4, rotate: 30, x: 300})
-    tl.to("#star1", {duration: 1, color: "white", y: "300px"});
+    tl.to("#star1", {duration: 1, color: "#ffa500", rotation: 360, x: "500px"})
+    .to("#star1", {duration: 1, color: "#0000ff", rotation: -360, x: "0px"});
 
     return tl;
 }
